a3056b8608813b0563da540019d3496981e1d309
Abdullah Nadeem <anadeem6301@gmail.com>
2024-04-28 17:10:00 +0500

    number_guessing_game_v.1

---
+++ index.js
@@ -0,0 +1,23 @@
+#! usr/bin/ env node
+import inquirer from "inquirer";
+let condition = false;
+const random = Math.floor(Math.random() * 50 + 1);
+while (condition != true) {
+    const answer = await inquirer.prompt([
+        {
+            name: "usernumber",
+            type: "number",
+            message: "Guess the number between 1-50: ",
+        }
+    ]);
+    if (answer.usernumber === random) {
+        console.log("Congraulation! you guessed right number");
+        condition = true;
+    }
+    else if (answer.usernumber < random) {
+        console.log("Too low. Try again.");
+    }
+    else {
+        console.log("Too high. Try again.");
+    }
+}
